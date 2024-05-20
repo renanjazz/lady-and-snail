4a298b3d32c3973a2eebc00f76986d410a24e8dd
Renan <renan.castro@dashlane.com>
2024-05-20 15:10:49 +0100

    more assets and first scripts

--- js/game.js
+++ js/game.js
@@ -3,21 +3,13 @@ class Game {
     this.startScreen = document.getElementById("game-intro");
     this.gameScreen = document.getElementById("game-screen");
     this.gameEndScreen = document.getElementById("game-end");
-    this.player1 = new Player(
+    this.player = new Player(
       this.gameScreen,
       50,
       50,
       400,
       250,
       "../images/ladyplayer.png"
-    );
-    this.player2 = new Player(
-      this.gameScreen,
-      50,
-      50,
-      400,
-      250,
-      "../images/snailplayer.png"
     );
     this.height = 600;
     this.width = 500;
@@ -28,7 +20,60 @@ class Game {
     this.gameIntervalId = null;
     this.gameLoopFrequency = 1000 / 60;
   }
+
+
+start() {
+    this.gameScreen.style.height = `${this.height}px`;
+    this.gameScreen.style.width = `${this.width}px`;
+    this.startScreen.style.display = "none";
+    this.gameScreen.style.display = "block";
+    this.gameIntervalId = setInterval(() => {
+      this.gameLoop();
+    }, this.gameLoopFrequency);
+  }
+  gameLoop() {
+    // console.log("inside the game loop");
+    this.update();
+    if (this.isGameOver) {
+      clearInterval(this.gameIntervalId);
+      this.gameOver();
+    }
+  }
+  update() {
+    // console.log("inside the update function");
+    this.player.move();
+
+    this.obstacles.forEach((oneObstacle, oneObstacleIndex) => {
+      oneObstacle.move();
+
+      const thereWasACollision = this.player.didCollide(oneObstacle);
+      if (thereWasACollision) {
+        this.obstacles.splice(oneObstacleIndex, 1);
+        oneObstacle.element.remove();
+        this.obstacles.push(new Obstacle(this.gameScreen));
+        this.lives -= 1;
+        if (this.lives === 0) {
+          this.isGameOver = true;
+        }
+        const livesElement = document.getElementById("lives");
+        livesElement.innerText = this.lives;
+      }
+
+      if (oneObstacle.top > 700) {
+        this.obstacles.splice(oneObstacleIndex, 1);
+        oneObstacle.element.remove();
+        //increase the score by 1
+        this.score += 1;
+        //always update the DOM to your new score
+        const scoreElement = document.getElementById("score");
+        scoreElement.innerText = this.score;
+        this.obstacles.push(new Obstacle(this.gameScreen));
+      }
+    });
+  }
+  gameOver() {
+    this.gameScreen.style.display = "none";
+    this.gameEndScreen.style.display = "block";
+  }
 }
-
-
 // same class of player, give different speed and lives
---
+++ js/player.js
@@ -0,0 +1,58 @@
+class Player {
+    constructor(gameScreen, width, height, top, left, playerImage) {
+      this.gameScreen = gameScreen;
+      this.left = left - width / 2;
+      this.top = top;
+      this.width = width;
+      this.height = height;
+      this.directionX = 0;
+      this.directionY = 0;
+      this.element = document.createElement("img");
+      this.element.src = playerImage;
+      this.element.style.position = "absolute";
+      this.element.style.top = `${this.top}px`;
+      this.element.style.left = `${this.left}px`;
+      this.element.style.height = `${this.height}px`;
+      this.element.style.width = `${this.width}px`;
+      this.gameScreen.appendChild(this.element);
+      this.honkSound = new Audio("../honk.wav");
+      this.honkSound.volume = 0.1;
+    }
+    move() {
+      this.left += this.directionX;
+      this.top += this.directionY;
+      if (this.left <= 35) {
+        this.left = 35;
+      }
+      if (this.left >= 465 - this.width) {
+        this.left = 465 - this.width;
+      }
+      this.updatePosition();
+    }
+    updatePosition() {
+      this.element.style.top = `${this.top}px`;
+      this.element.style.left = `${this.left}px`;
+    }
+    didCollide(obstacle) {
+      const playerRect = this.element.getBoundingClientRect();
+      const obstacleRect = obstacle.element.getBoundingClientRect();
+  
+      if (
+        playerRect.left < obstacleRect.right &&
+        playerRect.right > obstacleRect.left &&
+        playerRect.top < obstacleRect.bottom &&
+        playerRect.bottom > obstacleRect.top
+      ) {
+        //this adds the class of spin when we return true from a collision
+        this.element.classList.add("spin");
+        setTimeout(() => {
+          this.element.classList.remove("spin");
+        }, 500);
+        //this plays the honk sound on collision
+        this.honkSound.play();
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
